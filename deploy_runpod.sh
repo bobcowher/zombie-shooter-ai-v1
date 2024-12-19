8b5b82c08a66af9e6790f65866910e491ffabff7
Robert Cowher <bobcowher@gmail.com>
2024-12-19 17:27:49 -0600

    }

--- deploy_runpod.sh
+++ deploy_runpod.sh
@@ -4,16 +4,13 @@ RUNPOD_HOST=$1
 RUNPOD_PORT=$2
 RUNPOD_PROJECT_NAME=$3
 
-
-
 ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "pgrep -f python | xargs kill -9"
 ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "mkdir -p /app/${RUNPOD_PROJECT_NAME}"
 scp -P ${RUNPOD_PORT} *.py root@${RUNPOD_HOST}:/app/${RUNPOD_PROJECT_NAME}
 scp -r -P ${RUNPOD_PORT} images root@${RUNPOD_HOST}:/app/${RUNPOD_PROJECT_NAME}/images
 scp -r -P ${RUNPOD_PORT} sounds root@${RUNPOD_HOST}:/app/${RUNPOD_PROJECT_NAME}/sounds
 scp -P ${RUNPOD_PORT} requirements.txt root@${RUNPOD_HOST}:/app/${RUNPOD_PROJECT_NAME}
-ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "pip install -r /app/${RUNPOD_PROJECT_NAME}/requirements.txt" 
-ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "nohup python ./train.py &" 
-ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "nohup tensorboard --logdir='runs' --port=6007" 
-
+ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "cd /app/${RUNPOD_PROJECT_NAME} && pip install -r requirements.txt"
+ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "cd /app/${RUNPOD_PROJECT_NAME} && nohup bash -c 'python train.py > train.log 2>&1 &'"
+ssh root@${RUNPOD_HOST} -p ${RUNPOD_PORT} "cd /app/${RUNPOD_PROJECT_NAME} && nohup bash -c 'tensorboard --logdir=\"./runs\" --port=6007 > tensorboard.log 2>&1 &'"
 
